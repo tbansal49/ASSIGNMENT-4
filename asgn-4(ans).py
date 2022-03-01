f6e74dda3dbda3e2f7533195da007c15801f6293
tbansal49 <97750175+tbansal49@users.noreply.github.com>
2022-03-01 22:13:51 +0530

    Add files via upload

---
+++ asgn-4(ans).py
@@ -0,0 +1,71 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============== RESTART: C:/Users/Tanish/Documents/asgn-4(code).py ==============
+-------------1--------------
+Transfer disk from A to C
+Transfer disk from A to B
+Transfer disk from C to B
+Transfer disk from A to C
+Transfer disk from B to A
+Transfer disk from B to C
+Transfer disk from A to C
+-------------2(A)------------
+Using iteration-
+Give the no. of rows-4
+   1  
+
+  1  1  
+
+ 1  2  1  
+
+1  3  3  1  
+
+--------------2(B)-------------
+Using recursion-
+Please give no. of rows-4
+        1 
+
+       1 1 
+
+      1 2 1 
+
+     1 3 3 1 
+
+Done!
+-------------3(A)-----------
+Please give the first number-4
+Please give the second number-6
+The quotient is- 0
+The remainder is- 4
+[0, 4]
+Callable- True
+-------------3(B)-------------
+a is zero- False
+b is zero- False
+quotient is zero- True
+remainder is zero- False
+-------------3(C)-------------
+[0, 4, 4, 5, 6]
+The values greater than 4- [5, 6]
+-------------3(D)-------------
+{5, 6}
+-------------3(E)-------------
+The required immutable set frozenset({5, 6})
+-------------3(F)-------------
+The required max value is- 6
+The required hash value is- 6
+--------------4---------------
+Tony
+7
+Destructor called,The object is destroyed.
+-------------5(A)---------
+The updated salary of Mehak is- 70000
+-------------5(B)---------
+The record of Viren has been successfully deleted-
+-------------6-------------
+Please give a word-it
+Give a word-is
+Possible Words- ['it', 'ti']
+If Barbie's word lies in the list,then their friendship is real.
+Friendship is fake.
